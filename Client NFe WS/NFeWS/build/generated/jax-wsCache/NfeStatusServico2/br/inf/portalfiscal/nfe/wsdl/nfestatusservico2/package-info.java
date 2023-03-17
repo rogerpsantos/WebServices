92da8546d864ad28e628ff53664433098659479e
rogerpsantos <rogerpsantos3@gmail.com>
2023-03-17 15:34:06 +0000

    Delete Client NFe WS/NFeWS/build directory

--- Client NFe WS/NFeWS/build/generated/jax-wsCache/NfeStatusServico2/br/inf/portalfiscal/nfe/wsdl/nfestatusservico2/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Serviço destinado à consulta do status do serviço prestado pelo Portal da Secretaria de Fazenda Estadual.
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package br.inf.portalfiscal.nfe.wsdl.nfestatusservico2;
